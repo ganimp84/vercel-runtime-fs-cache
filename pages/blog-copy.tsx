04b67309b9daea93801f3323a09ecc1d345f676d
Gani Mohamed Parakadhullah <gani.git@gmail.com>
2021-12-31 01:15:20 +0800

    Added the second page
    - Added copy of blog page to confirm that it uses cached posts

---
+++ pages/blog-copy.tsx
@@ -0,0 +1,116 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Header from '../components/Header';
+import MainFeaturedPost from '../components/MainFeaturedPost';
+import FeaturedPost from '../components/FeaturedPost';
+import Main from '../components/Main';
+import Sidebar from '../components/Sidebar';
+import Footer from '../components/Footer';
+import { GetStaticProps, NextPage } from 'next';
+import { Post, Posts } from '../types/post';
+import * as cache from '../lib/cache'
+
+const sections = [
+    { title: 'Technology', url: '#' },
+    { title: 'Design', url: '#' },
+    { title: 'Culture', url: '#' },
+    { title: 'Business', url: '#' },
+    { title: 'Politics', url: '#' },
+    { title: 'Opinion', url: '#' },
+    { title: 'Science', url: '#' },
+    { title: 'Health', url: '#' },
+    { title: 'Style', url: '#' },
+    { title: 'Travel', url: '#' },
+];
+
+const sidebar = {
+    title: 'About',
+    description:
+        'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
+    archives: [
+        { title: 'March 2020', url: '#' },
+        { title: 'February 2020', url: '#' },
+        { title: 'January 2020', url: '#' },
+        { title: 'November 1999', url: '#' },
+        { title: 'October 1999', url: '#' },
+        { title: 'September 1999', url: '#' },
+        { title: 'August 1999', url: '#' },
+        { title: 'July 1999', url: '#' },
+        { title: 'June 1999', url: '#' },
+        { title: 'May 1999', url: '#' },
+        { title: 'April 1999', url: '#' },
+    ],
+    social: [
+        { name: 'GitHub', icon: GitHubIcon },
+        { name: 'Twitter', icon: TwitterIcon },
+        { name: 'Facebook', icon: FacebookIcon },
+    ],
+};
+
+const theme = createTheme();
+
+interface BlogProps {
+    posts: Posts
+}
+
+const BlogCopy: NextPage<BlogProps> = (props) => {
+    const mainFeaturedPost = props.posts.shift() as Post;
+    const featuredPosts = props.posts.slice(0, 3)
+    const otherPosts = props.posts.slice(4, 20)
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Container maxWidth="lg">
+                <Header title="Blog" sections={sections} />
+                <main>
+                    <MainFeaturedPost post={mainFeaturedPost} />
+                    <Grid container spacing={4}>
+                        {featuredPosts.map((post) => (
+                            <FeaturedPost key={post.title} post={post} />
+                        ))}
+                    </Grid>
+                    <Grid container spacing={5} sx={{ mt: 3 }}>
+                        <Main title="From the firehose" posts={otherPosts} />
+                        <Sidebar
+                            title={sidebar.title}
+                            description={sidebar.description}
+                            archives={sidebar.archives}
+                            social={sidebar.social}
+                        />
+                    </Grid>
+                </main>
+            </Container>
+            <Footer
+                title="Footer"
+                description="Something here to give the footer a purpose!"
+            />
+        </ThemeProvider>
+    );
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    console.table({ action: 'Generating/ Regenerating Todos page' });
+    let posts: Posts
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    if (cache.has(url)) {
+        console.table({ action: 'fetching posts', cached: true });
+        posts = cache.get<Posts>(url)
+    } else {
+        console.table({ action: 'fetching posts', cached: false });
+        posts = await fetch(url)
+            .then(response => response.json())
+        cache.set<Posts>(url, posts)
+    }
+
+    return {
+        props: { posts, revalidate: 24 * 60 * 60 }, // will be passed to the page component as props
+    }
+}
+
+export default BlogCopy
